5033991c5aeb308ce1590179dc3ca795a5ff525a
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-09-02 02:31:57 +0100

    🚧 Started intro dialogue and dialogue system

---
+++ GBJam8Unity/Assets/Scripts/DialgoueSystem/DialogueSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+	public TextPanel Text;
+
+	public IEnumerator WaitForUserInput()
+	{
+		while (true)
+		{
+			if (Input.GetKeyDown(KeyCode.X)
+				|| Input.GetKeyDown(KeyCode.C))
+			{
+				break;
+			}
+			yield return null;
+		}
+		AudioManager.Play(Game.Instance.Setup.NudgeSound);
+	}
+}
---
+++ GBJam8Unity/Assets/Scripts/StateMachineIntro.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using Utility;
+
+public class StateMachineIntro : StateMachineState
+{
+	public StateMachineIntro(Game game)
+		: base(game)
+	{
+
+	}
+
+	public IEnumerator IntroDialogue()
+	{
+		Setup.Dialogue.Text.SetText(Setup.IntroStyle1, "... hi?");
+
+		yield return new WaitForSeconds(5.0f);
+
+		Setup.Dialogue.Text.SetText(Setup.IntroStyle1, "Hello there! How are you doing?");
+
+		yield return new WaitForSeconds(5.0f);
+	}
+
+	public override IEnumerator StateRoutine()
+	{
+		Setup.SetActiveWorld(Setup.VoidWorld);
+
+		Setup.Dialogue.gameObject.SetActive(true);
+		Setup.CircleWipe.SetTime(1.0f);
+
+		foreach (float time in new TimedLoop(0.5f))
+		{
+			Setup.CircleWipe.SetTime(1.0f - time);
+			yield return null;
+		}
+
+		yield return StartCoroutine(IntroDialogue());
+
+
+		foreach (float time in new TimedLoop(0.5f))
+		{
+			Setup.CircleWipe.SetTime(time);
+			yield return null;
+		}
+		Setup.Dialogue.gameObject.SetActive(false);
+	}
+}
