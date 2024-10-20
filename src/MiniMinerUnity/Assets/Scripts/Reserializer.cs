ea82f436ac4253ffdda9f99b5d80173a03fcface
fydar <fydar.dev@gmail.com>
2024-10-20 12:40:24 +0100

    📦 Updating Unity to Unity 6.0.23f1

---
+++ src/MiniMinerUnity/Assets/Scripts/Reserializer.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public static class Reserializer
+{
+	[MenuItem("Toolkit/Reserialize")]
+	public static void Reserialize()
+	{
+		AssetDatabase.ForceReserializeAssets();
+	}
+}
+#endif
